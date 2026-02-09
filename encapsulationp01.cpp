f5b41590da5ec7c0b32c5253716a9e9bf77434a7
Nimra Fatima <n.fatima2029@gmail.com>
2026-02-09 23:53:41 +0500

    encapsulation01

--- encapsulationp01.cpp
+++ encapsulationp01.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class student{
+class Student{
     private:
-    int marks ;
+    int marks = 0;
+    string name ;
     public:
-    string name = "Nimra Fatima" ;
     void setMarks(int m){
+        if(m >= 0 && m <= 100){
         marks = m;
+    }
     }
     int getMarks(){
         return marks;
     }
 };
 int main (){
-    student m1;
+    Student m1;
     m1.setMarks(40);
     cout <<m1.getMarks() << endl;
 }
